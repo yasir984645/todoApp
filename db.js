f181aca2f7a31173dd5c8f709353c54e85f04bcf
yasir984645 <yali984645@gmail.com>
2026-02-12 19:21:54 +0400

    Removed .env file

--- db.js
+++ db.js
@@ -1,16 +1,15 @@
+// db.js
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL, // <- use full URL
+  ssl: {
+    rejectUnauthorized: false, // needed for Render
+  },
 });
 
 pool.connect()
-  .then(() => console.log("Connected to PostgreSQL database"))
+  .then(() => console.log("Connected to Render PostgreSQL database"))
   .catch(err => console.error("DB connection error:", err));
-
 
 module.exports = pool;
