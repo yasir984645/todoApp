5e68fc6abae5a408d586017ee61e68170fe0a9bc
yasir984645 <yali984645@gmail.com>
2026-02-12 18:44:49 +0400

    sadfsdf

--- db.js
+++ db.js
@@ -8,4 +8,9 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
+pool.connect()
+  .then(() => console.log("Connected to PostgreSQL database"))
+  .catch(err => console.error("DB connection error:", err));
+
+
 module.exports = pool;
